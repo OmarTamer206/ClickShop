8798d1996a0a5088090e135eb157dc52afc52c19
OMEGAT206 <131653790+OMEGAT206@users.noreply.github.com>
2023-12-12 20:48:14 +0200

    Homepage Confirmed

---
+++ ClickShopProject/HomePageScript.js
@@ -0,0 +1,36 @@
+// Category List Start
+
+document.addEventListener("DOMContentLoaded", function () {
+    var menuItems = document.querySelectorAll(".menu-item");
+
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener("mouseover", function () {
+            // Hide all submenus
+            menuItems.forEach(function (item) {
+                // item.classList.remove("active");
+                var submenu = item.querySelector(".submenu");
+                if (submenu) {
+                    submenu.style.display = "none";
+                }
+            });
+
+            // Show the submenu of the hovered item
+            // menuItem.classList.add("active");
+            var submenu = menuItem.querySelector(".submenu");
+            if (submenu) {
+                submenu.style.display = "flex";
+            }
+        });
+
+        menuItem.addEventListener("mouseout", function () {
+            // Hide the submenu when mouse leaves the menu item
+            // menuItem.classList.remove("active");
+            var submenu = menuItem.querySelector(".submenu");
+            if (submenu) {
+                submenu.style.display = "none";
+            }
+        });
+    });
+});
+
+// Category List End
