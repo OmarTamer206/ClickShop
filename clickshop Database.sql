c39ad509e9b84de9e7fcc4d5510cd90b0ed6786d
OMEGAT206 <131653790+OMEGAT206@users.noreply.github.com>
2023-12-20 04:18:14 +0200

    Create clickshop Database.sql

---
+++ clickshop Database.sql
@@ -0,0 +1,409 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 20, 2023 at 03:17 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `clickshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `a_id` int(11) NOT NULL,
+  `a_name` varchar(255) NOT NULL,
+  `a_email` varchar(255) NOT NULL,
+  `a_phoneNumber` varchar(20) DEFAULT NULL,
+  `a_gender` varchar(10) DEFAULT NULL,
+  `a_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`a_id`, `a_name`, `a_email`, `a_phoneNumber`, `a_gender`, `a_password`) VALUES
+(1, 'Omar Tamer', 'omartamer94@gmail.com', '01553072551', 'male', 'omaromar');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billingaddress`
+--
+
+CREATE TABLE `billingaddress` (
+  `ba_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `ba_city` varchar(255) NOT NULL,
+  `ba_street` varchar(255) NOT NULL,
+  `ba_buildingNumber` int(11) NOT NULL,
+  `ba_floor` int(11) NOT NULL,
+  `ba_apartmentNumber` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand`
+--
+
+CREATE TABLE `brand` (
+  `b_id` int(11) NOT NULL,
+  `b_name` varchar(255) NOT NULL,
+  `b_profilePic` varchar(255) DEFAULT NULL,
+  `b_businessDescription` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cart_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `p_id` int(11) NOT NULL,
+  `cart_qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(255) NOT NULL,
+  `cat_description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `c_id` int(11) NOT NULL,
+  `c_profilePic` varchar(255) DEFAULT NULL,
+  `c_name` varchar(255) NOT NULL,
+  `c_email` varchar(255) NOT NULL,
+  `c_password` varchar(255) NOT NULL,
+  `c_pNumber` varchar(20) DEFAULT NULL,
+  `c_gender` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`c_id`, `c_profilePic`, `c_name`, `c_email`, `c_password`, `c_pNumber`, `c_gender`) VALUES
+(1, NULL, 'Omar Tamer', 'omartamer206@outlook.com', 'omaromar', '01553072551', 'male'),
+(2, NULL, 'Kapo', 'kapo123@gmail.com', 'kapokapo', '01019990785', 'male'),
+(3, NULL, 'Omar Tamer', 'omartamer202@outlook.com', 'omaromar', '123123123', 'male');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `f_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `f_date` datetime NOT NULL,
+  `f_message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderedproducts`
+--
+
+CREATE TABLE `orderedproducts` (
+  `op_id` int(11) NOT NULL,
+  `o_id` int(11) NOT NULL,
+  `p_id` int(11) NOT NULL,
+  `op_qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `o_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `o_date` datetime NOT NULL,
+  `ba_id` int(11) NOT NULL,
+  `o_totalPrice` decimal(10,2) NOT NULL,
+  `o_rating` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `p_id` int(11) NOT NULL,
+  `p_image` varchar(255) DEFAULT NULL,
+  `p_name` varchar(255) NOT NULL,
+  `p_price` decimal(10,2) NOT NULL,
+  `p_description` text DEFAULT NULL,
+  `p_qty` int(11) NOT NULL,
+  `b_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `s_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller`
+--
+
+CREATE TABLE `seller` (
+  `s_id` int(11) NOT NULL,
+  `s_name` varchar(255) NOT NULL,
+  `s_profilePic` varchar(255) DEFAULT NULL,
+  `s_businessDescription` text DEFAULT NULL,
+  `s_email` varchar(255) NOT NULL,
+  `s_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `seller`
+--
+
+INSERT INTO `seller` (`s_id`, `s_name`, `s_profilePic`, `s_businessDescription`, `s_email`, `s_password`) VALUES
+(1, 'Omar Tamer', NULL, 'Sells Electronic Devices', 'omega121@gmail.com', 'omaromar');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`a_id`),
+  ADD UNIQUE KEY `a_email` (`a_email`);
+
+--
+-- Indexes for table `billingaddress`
+--
+ALTER TABLE `billingaddress`
+  ADD PRIMARY KEY (`ba_id`),
+  ADD KEY `c_id` (`c_id`);
+
+--
+-- Indexes for table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`b_id`),
+  ADD UNIQUE KEY `b_name` (`b_name`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cart_id`),
+  ADD KEY `c_id` (`c_id`),
+  ADD KEY `p_id` (`p_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`c_id`),
+  ADD UNIQUE KEY `c_email` (`c_email`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`f_id`),
+  ADD KEY `c_id` (`c_id`);
+
+--
+-- Indexes for table `orderedproducts`
+--
+ALTER TABLE `orderedproducts`
+  ADD PRIMARY KEY (`op_id`),
+  ADD KEY `o_id` (`o_id`),
+  ADD KEY `p_id` (`p_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`o_id`),
+  ADD KEY `c_id` (`c_id`),
+  ADD KEY `ba_id` (`ba_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`p_id`),
+  ADD KEY `b_id` (`b_id`),
+  ADD KEY `s_id` (`s_id`),
+  ADD KEY `cat_id` (`cat_id`);
+
+--
+-- Indexes for table `seller`
+--
+ALTER TABLE `seller`
+  ADD PRIMARY KEY (`s_id`),
+  ADD UNIQUE KEY `s_email` (`s_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `billingaddress`
+--
+ALTER TABLE `billingaddress`
+  MODIFY `ba_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `f_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orderedproducts`
+--
+ALTER TABLE `orderedproducts`
+  MODIFY `op_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `seller`
+--
+ALTER TABLE `seller`
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `billingaddress`
+--
+ALTER TABLE `billingaddress`
+  ADD CONSTRAINT `billingaddress_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`);
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`p_id`) REFERENCES `product` (`p_id`);
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`);
+
+--
+-- Constraints for table `orderedproducts`
+--
+ALTER TABLE `orderedproducts`
+  ADD CONSTRAINT `orderedproducts_ibfk_1` FOREIGN KEY (`o_id`) REFERENCES `orders` (`o_id`),
+  ADD CONSTRAINT `orderedproducts_ibfk_2` FOREIGN KEY (`p_id`) REFERENCES `product` (`p_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`ba_id`) REFERENCES `billingaddress` (`ba_id`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`b_id`) REFERENCES `brand` (`b_id`),
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `seller` (`s_id`),
+  ADD CONSTRAINT `product_ibfk_3` FOREIGN KEY (`cat_id`) REFERENCES `category` (`cat_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
