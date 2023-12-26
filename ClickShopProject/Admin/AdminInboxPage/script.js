87c9101c2a38637008224b0107e56ce173c56c39
OMEGAT206 <131653790+OMEGAT206@users.noreply.github.com>
2023-12-26 16:37:31 +0200

    checkpoint

--- ClickShopProject/Admin/AdminInboxPage/script.js
+++ ClickShopProject/Admin/AdminInboxPage/script.js
@@ -1,5 +1,77 @@
 let feedbackTable = document.querySelector(".allFeedbacks");
 document.addEventListener("DOMContentLoaded", async () => {
+    let authState = await checkAuth();
+    let userType = await getUserType();
+
+    if (authState["state"]) {
+        if (userType["type"] == "customer") {
+            window.location.href = "../../index.html";
+        } else if (userType["type"] == "seller") {
+            window.location.href = "../../Seller/SellerPage/sellerhome.html";
+        }
+    } else {
+        window.location.href = "../../LoginPage.html";
+    }
+    async function checkAuth() {
+        const data = {
+            functionName: "checkAuth",
+        };
+
+        console.log(data);
+
+        try {
+            console.log(JSON.stringify(data));
+
+            let response = await fetch("../../PHP/AccountManagement.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            console.log("hi");
+            // console.log(response.json());
+            console.log("hi");
+            // console.log(response.json());
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    }
+
+    async function getUserType() {
+        const data = {
+            functionName: "getUserType",
+        };
+
+        console.log(data);
+
+        try {
+            console.log(JSON.stringify(data));
+
+            let response = await fetch("../../PHP/AccountManagement.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            console.log("hi");
+            // console.log(response.json());
+            console.log("hi");
+            // console.log(response.json());
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    }
+
     let feedbacks = await getFeedbacks();
     for (const feedback in feedbacks["feedbacks"]) {
         feedbackTable.innerHTML += `
