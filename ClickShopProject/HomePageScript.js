7c5f5d920f51f59adbc6c05a2e47090d113ee30d
OMEGAT206 <131653790+OMEGAT206@users.noreply.github.com>
2023-12-27 07:18:09 +0200

    MINIMAL DONE

--- ClickShopProject/HomePageScript.js
+++ ClickShopProject/HomePageScript.js
@@ -34,3 +34,74 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Category List End
+
+document.addEventListener("DOMContentLoaded", async () => {
+    let authState = await checkAuth();
+    let userType = await getUserType();
+    if (authState["state"]) {
+        if (userType["type"] == "admin") {
+            window.location.href = "Admin/adminHome/adminhome.html";
+        } else if (userType["type"] == "seller") {
+            window.location.href = "Seller/SellerPage/sellerhome.html";
+        }
+    }
+    async function checkAuth() {
+        const data = {
+            functionName: "checkAuth",
+        };
+
+        console.log(data);
+
+        try {
+            console.log(JSON.stringify(data));
+
+            let response = await fetch("PHP/AccountManagement.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            console.log("hi");
+            // console.log(response.json());
+            console.log("hi");
+            // console.log(response.json());
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    }
+
+    async function getUserType() {
+        const data = {
+            functionName: "getUserType",
+        };
+
+        console.log(data);
+
+        try {
+            console.log(JSON.stringify(data));
+
+            let response = await fetch("PHP/AccountManagement.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            console.log("hi");
+            // console.log(response.json());
+            console.log("hi");
+            // console.log(response.json());
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    }
+});
--- ClickShopProject/LoginPage.js
+++ ClickShopProject/LoginPage.js
@@ -192,3 +192,76 @@ async function logInUser() {
 }
 
 // Login End
+
+document.addEventListener("DOMContentLoaded", async () => {
+    let authState = await checkAuth();
+    let userType = await getUserType();
+    if (authState["state"]) {
+        if (userType["type"] == "admin") {
+            window.location.href = "Admin/adminHome/adminhome.html";
+        } else if (userType["type"] == "seller") {
+            window.location.href = "Seller/SellerPage/sellerhome.html";
+        } else if (userType["type"] == "customer") {
+            window.location.href = "index.html";
+        }
+    }
+    async function checkAuth() {
+        const data = {
+            functionName: "checkAuth",
+        };
+
+        console.log(data);
+
+        try {
+            console.log(JSON.stringify(data));
+
+            let response = await fetch("PHP/AccountManagement.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            console.log("hi");
+            // console.log(response.json());
+            console.log("hi");
+            // console.log(response.json());
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    }
+
+    async function getUserType() {
+        const data = {
+            functionName: "getUserType",
+        };
+
+        console.log(data);
+
+        try {
+            console.log(JSON.stringify(data));
+
+            let response = await fetch("PHP/AccountManagement.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            console.log("hi");
+            // console.log(response.json());
+            console.log("hi");
+            // console.log(response.json());
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    }
+});
